a6fb1bf874f5b4fa718f2c3f082f7edc7a7c92d3
Lewissp312 <lewissp@live.co.uk>
2024-08-01 12:34:25 +0100

    Added start and end screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,18 +6,38 @@ public class GameManager : MonoBehaviour
     public float xBound = 40;
     public float zBound = 40;
     public GameObject[] enemies;
-    
+    public GameObject startScreen;
+    public GameObject endScreen; 
     private Vector3 posToSpawnOn;
+    private bool isGameActive;
+    private int numOfEnemies;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating(nameof(SpawnEnemies),1,10);
+        isGameActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame(){
+        isGameActive = true;
+        startScreen.SetActive(false);
+        InvokeRepeating(nameof(SpawnEnemies),1,10);
+    }
+
+    public void EndGame(){
+        isGameActive = false;
+        DestroyAllEnemies("Enemy 1");
+        DestroyAllEnemies("Enemy 2");
+        DestroyAllEnemies("Enemy 3");
+        DestroyAllEnemies("Enemy 4");
+        endScreen.SetActive(true);
+        CancelInvoke();
     }
     public Vector3 GetMouseOnBoardPosition(){
         Ray ray;
@@ -36,6 +56,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public bool GetIsGameActive(){
+        return isGameActive;
+    }
+
     void SpawnEnemies(){
         GameObject enemyToSpawn = enemies[Random.Range(0,4)];
         if (enemyToSpawn.CompareTag("Enemy 1") || enemyToSpawn.CompareTag("Enemy 4")){
@@ -46,5 +70,14 @@ public class GameManager : MonoBehaviour
         }
         posToSpawnOn = new Vector3(Random.Range(-16.5f,20.6f),-0.119999997f,Random.Range(-21.3f,15.8f));
         Instantiate(enemyToSpawn,posToSpawnOn,enemyToSpawn.transform.rotation);
+    }
+
+    void DestroyAllEnemies(System.String enemyType){
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyType);
+        if (enemies.Length>0){
+            foreach(GameObject enemy in enemies){
+                Destroy(enemy);
+            }
+        }
     }
 }
